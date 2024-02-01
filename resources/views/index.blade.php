c4b2b5563afccbbabb50a31716de0feab1cf8293
Duhell <ryanvillarma148@gmail.com>
2024-02-01 18:41:18 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,9 @@
     <meta name="robots" content="index">
     <meta http-equiv="content-language” content="en-us">
     <title>{{ $title ?? "Visual Angle Advertising Company"}}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     @livewireStyles
     @vite(['./resources/css/app.css','./resources/js/app.js'])
 </head>
