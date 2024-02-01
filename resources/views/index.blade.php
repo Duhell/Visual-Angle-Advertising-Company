e31dc6a41198e97eaae1e3e8c82b49d13c29219d
Duhell <ryanvillarma148@gmail.com>
2024-02-01 19:03:03 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     @livewireStyles
-    @vite(['./resources/css/app.css','./resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
     @include('includes.nav')
